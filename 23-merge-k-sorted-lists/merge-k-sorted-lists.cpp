df163bbb37ce1d4031481f6267cb31657dd73c3c
SanskratiGupta <sanskratigupta94@gmail.com>
2026-09-23 19:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (82.32%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+
+        for (ListNode* head : lists) {
+            if (head)
+                pq.push(head);
+        }
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (!pq.empty()) {
+            ListNode* curr = pq.top();
+            pq.pop();
+
+            tail->next = curr;
+            tail = curr;
+
+            if (curr->next)
+                pq.push(curr->next);
+        }
+
+        return dummy.next;
+    }
+};
